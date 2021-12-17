4904cb03e5c3bd29d8902acec994a4f9a9495c45
unknown <fcutufia@hotmail.com>
2021-12-16 22:14:09 -0300

    homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -66,11 +66,12 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-  for (var i = 0; i < array.length; i++ )
-  if (array[i] === elemento){
-    return true
+  for(var i = 0; i < array.length; i++) {
+    if (array[i] === elemento) {
+    return true;
+    }
   }
-  else return false
+  return false
 }
 
 
@@ -78,6 +79,10 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var suma = 0
+  for(var i = 0; i <numeros.length; i++) 
+  suma = suma + numeros[i]
+  return suma
 }
 
 
@@ -85,6 +90,11 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  var test = 0
+  for(var i = 0; i <resultadosTest.length; i++)
+  test = test + resultadosTest[i]
+  return test/[i]
+
 }
 
 
